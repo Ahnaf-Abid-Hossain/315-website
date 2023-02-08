9bfefc778969a08f95a0c00978646e1c25ef502f
Ahnaf-hossain <88912262+Ahnaf-hossain@users.noreply.github.com>
2023-02-07 23:35:49 -0600

    final

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,46 @@
 
-function change(){
-	button1 = document.getElementsByClassName("no-button");
-	button1.style.display = "none";
-}
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	// var theme = document.getElementsByTagName('link')[0];
+
+	// // Change the value of href attribute 
+	// // to change the css sheet.
+	// if (theme.getAttribute('href') == 'style.css') {
+	// 	theme.setAttribute('href', 'style2.css');
+	// } else {
+	// 	theme.setAttribute('href', 'style.css');
+	// }
+	const check = document.getElementById("check")
+
+	if(localStorage.getItem('darkMode') ===null){
+		localStorage.setItem('darkMode',"false")
+	}
+
+	// const link = document.createElement('link');
+	// link.rel = 'stylesheet';
+	// document.getElementsByTagName('HEAD')[0].appendChild(link);
+
+	checkStatus()
+
+	function checkStatus(){
+		const link = document.getElementsByTagName('link')[0];
+		if(localStorage.getItem('darkMode')==="true"){
+			check.checked = true;
+			link.href = "./style2.css";
+		}else{
+			check.checked = false;
+			link.href='./style.css';
+		}
+	}
+	
+	function changeStatus(){
+		const link = document.getElementsByTagName('link')[0];
+		if(localStorage.getItem('darkMode')==="true"){
+			localStorage.setItem('darkMode' , "false");
+			link.href ='./style.css';
+		}else{
+			localStorage.setItem('darkMode', "true");
+			link.href = './style2.css'
+		}
+	}
---
+++ public_html/special/script.js
@@ -0,0 +1,5 @@
+
+function change(){
+	button1 = document.getElementsByClassName("no-button");
+	button1.style.display = "none";
+}
